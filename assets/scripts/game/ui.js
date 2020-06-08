2e3f83b3ffc683fba185ab4c5286a7e77a8ad6bc
kbini28 <kbini28@gmail.com>
2020-06-08 13:59:21 -0400

    Show/Hide Sign In
    Added classes to hide sign in after completion and vice-versa

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,44 +2,50 @@ const store = require('./../store')
 
 const signUpSuccess = function (response) {
   $('form').trigger('reset')
-  $('#message').text('Sign up successful! Welcome ' + response.user.email + '!').show()
+  $('#message').text('Sign up successful! Welcome ' + response.user.email + '!').show().removeClass().addClass('success')
+  $('#logged-out-view').hide()
+  $('.hidden').removeClass('hidden')
 }
 
 const signUpFailure = function () {
   $('form').trigger('reset')
-  $('#message').text('Sign up failed.').show()
+  $('#message').text('Sign up failed.').show().removeClass().addClass('failure')
 }
 
 const signInSuccess = function (response) {
   $('form').trigger('reset')
-  $('#message').text('Welcome back ' + response.user.email + '!').show()
+  $('#message').text('Welcome back ' + response.user.email + '!').show().removeClass().addClass('success')
+  $('#logged-out-view').hide()
+  $('.hidden').removeClass('hidden')
   store.user = response.user
 }
 
 const signInFailure = function () {
   $('form').trigger('reset')
-  $('#message').text('Sign in failed. Try again.').show()
+  $('#message').text('Sign in failed. Try again.').show().removeClass().addClass('failure')
 }
 
 const changePassSuccess = function (response) {
   $('form').trigger('reset')
-  $('#message').text('Password updated!').show()
+  $('#message').text('Password updated!').show().removeClass().addClass('success')
   store.user.password = response.user.password
 }
 
 const changePassFailure = function () {
   $('form').trigger('reset')
-  $('#message').text('Change password failed.').show()
+  $('#message').text('Change password failed.').show().removeClass().addClass('failure')
 }
 
 const signOutSuccess = function () {
   $('form').trigger('reset')
-  $('#message').text('Goodbye!').show()
+  $('#message').text('Goodbye!').show().removeClass().addClass('success')
+  $('#logged-out-view').show()
+  $('main').addClass('hidden')
 }
 
 const signOutFailure = function () {
   $('form').trigger('reset')
-  $('#message').text('Sign out failed. You are stuck here with us!').show
+  $('#message').text('Sign out failed. You are stuck here with us!').show().removeClass().addClass('failure')
 }
 
 module.exports = {
